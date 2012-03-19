0997ee39abdcbe51307eb7ae34245270c9925d0a
Jeff Zych <jlzych@gmail.com>
2012-03-19 09:57:29 -0700

    Using directory indexes and absolute links again.

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ set :blog_permalink, "projects/:title.html"
 set :blog_layout_engine, "haml"
 # set :blog_summary_separator, /READMORE/
 # set :blog_summary_length, 500
+set :build_dir, "../jlzych.github.com"
 
 page "/feed.xml", :layout => false
-# activate :directory_indexes
-activate :relative_assets
+activate :directory_indexes
 
 # Build-specific configuration
 configure :build do
