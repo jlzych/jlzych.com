0da5a87a248dbde1af148410b5996566fe2842a3
Jeff Zych <jlzych@gmail.com>
2012-06-30 19:34:43 -0700

    Took a first stab at an HTML resume.

--- config.rb
+++ config.rb
@@ -3,6 +3,7 @@ set :blog_permalink, "projects/:title.html"
 set :blog_layout_engine, "haml"
 # set :blog_summary_separator, /READMORE/
 # set :blog_summary_length, 500
+
 set :build_dir, "../jlzych.github.com"
 
 page "/feed.xml", :layout => false
@@ -13,10 +14,10 @@ configure :build do
   #set :http_prefix, "/~jlzych"
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   activate :cache_buster
 end
